7eab9ee4fcb4de0787380d210613c8ab4b1e5f9e
andreguimaraesj <dev.detto@gmail.com>
2023-06-25 20:26:36 -0300

    ajustes paginaçao

--- src/app.js
+++ src/app.js
@@ -8,22 +8,58 @@ app.use(express.json());
 const tweets = [];
 const usuarios = [];
 
-
 app.get("/tweets", (req,res) =>{
-    let ultimosTweets = []
-    ultimosTweets = tweets.slice(-10);
+    const page = parseInt(req.query.page);
+    if(page === undefined){
+        let ultimosTweets = []
+        ultimosTweets = tweets.slice(-10);
 
-    const tweetsComAvatar = ultimosTweets.map(tweet => {
-        const usuarioEncontrado = usuarios.find(usuario => usuario.username === tweet.username);
-        
-        return {
-            username: tweet.username,
-            avatar: usuarioEncontrado.avatar,
-            tweet: tweet.tweet
-        };
-    });
+        const tweetsComAvatar = ultimosTweets.map(tweet => {
+            const usuarioEncontrado = usuarios.find(usuario => usuario.username === tweet.username);
+            
+            return {
+                username: tweet.username,
+                avatar: usuarioEncontrado.avatar,
+                tweet: tweet.tweet
+            };
+        });
 
-    res.send(tweetsComAvatar);
+        res.send(tweetsComAvatar);
+        return;
+    }
+    if(page<1){
+        res.status(400).send("Informe uma página válida!");
+        return;
+    }
+    if(page>0){
+        const tweetsReverse = [...tweets].reverse();
+
+        const indiceComeco = (page - 1) * 10;
+        const indiceFinal = indiceComeco + 9;
+
+        const limiteArray = tweetsReverse.slice(indiceComeco, indiceFinal);
+
+        const tweetsComAvatar = limiteArray.map(tweet => {
+            const usuarioEncontrado = usuarios.find(usuario => usuario.username === tweet.username);
+            
+            return {
+                username: tweet.username,
+                avatar: usuarioEncontrado.avatar,
+                tweet: tweet.tweet
+            };
+        });
+
+        res.send(tweetsComAvatar);
+        return;
+    }
+});
+
+app.get("/tweets/:USERNAME", (req,res)=>{
+    const { USERNAME } = req.params
+
+    const userTweets = tweets.filter(tweet => tweet.username === USERNAME);
+
+    res.send(userTweets);
 });
 
 app.post("/sign-up",(req,res)=>{
@@ -49,25 +85,27 @@ app.post("/sign-up",(req,res)=>{
 })
 
 app.post("/tweets", (req,res)=>{
-    const {username, tweet} = req.body;
+    const {tweet} = req.body;
+    const {user} = req.headers;
 
-    if(!username || !tweet){
+
+    if(!user || !tweet){
         res.status(400).send("Todos os campos são obrigatórios!")
         return
     }
 
-    if(typeof username !== "string" || typeof tweet !== "string"){
+    if(typeof user !== "string" || typeof tweet !== "string"){
         res.status(400).send("Todos os campos são obrigatórios!")
         return
     }
 
-    if (!usuarios.includes(username)) {
-        res.status(401).send("“UNAUTHORIZED”");
+    if (!usuarios.find(usuario => usuario.username === user)) {
+        res.status(401).send("UNAUTHORIZED");
         return;
     }
 
     const newTweet = {
-        username: username,
+        username: user,
         tweet: tweet
     }
 
